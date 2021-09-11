1595c5df66d8fbe7d0da05a243181c1b3f3466bf
thejedicoder <jedicoder@yahoo.com>
2021-09-11 16:22:55 -0400

    Added the trips component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TripsComponent} from "./trips/trips.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'trips', component: TripsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TripsComponent } from './trips/trips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripsComponent
   ],
   imports: [
     BrowserModule,
